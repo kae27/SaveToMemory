6e807041ab521c949c721e7214c6862f3084ce87
visanuPhalcon <s5404061636321@email.kmutnb.ac.th>
2017-08-04 16:00:12 +0700

    Initial commit

---
+++ app/src/main/java/com/kae27/savetomemory/MainActivity.java
@@ -0,0 +1,13 @@
+package com.kae27.savetomemory;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
